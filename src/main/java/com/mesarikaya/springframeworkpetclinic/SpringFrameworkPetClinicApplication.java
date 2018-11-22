1f559fdd51d12060021e2549c3e82462c660b297
mesarikaya <mesarikaya@gmail.com>
2018-11-22 15:19:37 +0100

    First Template

---
+++ src/main/java/com/mesarikaya/springframeworkpetclinic/SpringFrameworkPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.mesarikaya.springframeworkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkPetClinicApplication.class, args);
+    }
+}
