1df9c3a1d430c57995004063200859ba31de9cc6
mesarikaya <mesarikaya@gmail.com>
2018-11-23 15:53:07 +0100

    Adding Vet Clinic view page. closes #7

---
+++ pet-clinic-web/src/main/java/com/mesarikaya/springframeworkpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.mesarikaya.springframeworkpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+
+}
