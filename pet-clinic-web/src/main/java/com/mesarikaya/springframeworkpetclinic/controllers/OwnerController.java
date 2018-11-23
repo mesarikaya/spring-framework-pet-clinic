55c2c5df3d33803766d972a6bea25b62d355cf0c
mesarikaya <mesarikaya@gmail.com>
2018-11-23 15:57:07 +0100

    Adding List of Owners view page.

---
+++ pet-clinic-web/src/main/java/com/mesarikaya/springframeworkpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.mesarikaya.springframeworkpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index.html"})
+    public String listOwners(){
+
+        return "/owners/index";
+    }
+}
