3f27497f1a128e23a9aeb4ac2eadbc199dd764e0
mesarikaya <mesarikaya@gmail.com>
2018-11-24 14:45:57 +0100

    List All Vets on the Index Page closes #15

--- pet-clinic-web/src/main/java/com/mesarikaya/springframeworkpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mesarikaya/springframeworkpetclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.mesarikaya.springframeworkpetclinic.controllers;
 
+import com.mesarikaya.springframeworkpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index.html"})
-    public String listVets(){
+    private VetService vetService;
 
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
